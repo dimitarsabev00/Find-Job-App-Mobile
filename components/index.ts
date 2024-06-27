0bb618b826fe780ffb2083648c3a8d07f8fc896f
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-27 17:01:51 +0300

    Added Home Screen UI + JSearch API Integration

---
+++ components/index.ts
@@ -0,0 +1,16 @@
+export { default as ScreenHeaderBtn } from "./common/header/ScreenHeaderBtn";
+
+// home screen
+export { default as Welcome } from "./home/welcome/Welcome";
+export { default as NearbyJobs } from "./home/nearby/NearbyJobs";
+export { default as PopularJobs } from "./home/popular/PopularJobs";
+
+// job details screen
+// import Company from "./jobdetails/company/Company";
+// import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+// import { default as JobAbout } from "./jobdetails/about/About";
+// import { default as JobFooter } from "./jobdetails/footer/Footer";
+// import Specifics from "./jobdetails/specifics/Specifics";
+
+// common
+export { default as NearbyJobCard } from "./common/cards/nearby/NearbyJobCard";
